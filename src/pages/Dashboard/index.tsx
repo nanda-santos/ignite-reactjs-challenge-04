f90a48e10b1b00cbdaf5724a7ff9bc9142b95cda
Nanda <nanda.santos4028@gmail.com>
2022-10-24 11:31:13 -0300

    Convert Food Component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import Header from '../../components/Header';
 import api from '../../services/api';
-import Food from '../../components/Food';
+import { Food } from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
@@ -92,7 +92,7 @@ export function Dashboard() {
             <Food
               key={food.id}
               food={food}
-              handleDelete={handleDeleteFood}
+              handleDeleteFood={handleDeleteFood}
               handleEditFood={handleEditFood}
             />
           ))}
